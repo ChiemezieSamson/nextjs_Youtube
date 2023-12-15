567532b1871f0f06a10c965ce2053b687643476c
Nebeolisa chiemezie Samson <daddygreenarrow44@gmail.com>
2023-12-15 16:42:11 +0900

    Still on hadling error

--- src/app/products/[productid]/layout.tsx
+++ src/app/products/[productid]/layout.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count)
+}
+
 const ProductDetailsLayOut = ({children}: {children: React.ReactNode}) => {
+  const random = getRandomInt(2)
+
+
+  if(random === 1) {
+    throw new Error("Error loading product")
+  }
+
   return (
     <div>
       {children}
--- src/app/products/[productid]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productid]/reviews/[reviewId]/page.tsx
@@ -1,13 +1,23 @@
+"use client"
 import { notFound } from 'next/navigation'
 import React from 'react'
 
-const ReviewDetails = ({params}: {params: {
-  reviewId: string,
-  productid: string
-}}) => {
+const getRandomInt = (count: number) => {
+  return Math.floor(Math.random() * count)
+}
+
+const ReviewDetails = ({params}: {params: {reviewId: string, productid: string}}) => {
+  const random = getRandomInt(2)
+  
+
   if (parseInt(params.reviewId) > 1000) {
     notFound();
   } 
+
+  // if(random === 1) {
+  //   throw new Error("Error loading reiview")
+  // }
+
   return (
     <div>
       Review {params.reviewId} for product {params.productid}
